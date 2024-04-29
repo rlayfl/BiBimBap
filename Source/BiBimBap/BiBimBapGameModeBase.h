f3be508e6bb1b3ba59f9a75e79a78f9e371ced8e
rlayfl <rlayfl@essex.ac.uk>
2024-04-29 13:35:44 +0100

    First setup

---
+++ Source/BiBimBap/BiBimBapGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BiBimBapGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIBIMBAP_API ABiBimBapGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
